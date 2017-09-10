89b1a13a435af063475d14661ecee2a5c0eea6a0
Mario Solorzano <marioxcore1@gmail.com>
2017-09-09 20:37:52 -0700

    version 2.0 in the hash table

--- hashTable.js
+++ hashTable.js
@@ -1,40 +1,86 @@
 class HashTable {
   constructor() {
     this.valueArray = [];
+    this.maxLength = 701; // NOTE best to use a prime number
   }
 
   add(key, value) {
-    if (typeof key !== 'string') {
-      return new Error('Key must be a string!');
+    const index = this.getIndex(key);
+
+    if(this.valueArray[index] !== undefined) {
+      const exists = this.valueArray[index].reduce((doesExist, entry) => {
+        if(entry[0] === key) {
+          return true;
+        }
+      }, false);
+
+      if(exists) {
+        return new Error('Already exists!');
+      }
+
+      return this.valueArray.push([key, value]);
     }
 
-    const index = this.generateNumberFromString(key);
-    this.valueArray[index] = value;
+    return this.valueArray[index] = [[key, value]];
   }
 
   remove(key) {
-    if (typeof key !== 'string') {
-      return new Error('Key must be a string!');
+    const index = this.getIndex(key);
+
+    if (!this.valueArray[index]) {
+      return undefined;
     }
 
-    this.valueArray[this.generateNumberFromString(key)] = undefined;
+    for(let i = 0; i < this.valueArray[index].length; i++) {
+      if(key === this.valueArray[index][i][0]) {
+        this.valueArray[index][i] = undefined;
+
+        this.valueArray[index] = this.valueArray[index].splice();
+
+        if(this.valueArray[index].length === 0) {
+          this.valueArray[index] = undefined;
+        }
+        return true;
+      }
+    }
   }
 
   get(key) {
-    if (typeof key !== 'string') {
-      return new Error('Key must be a string!');
+    const index = this.getIndex(key);
+
+    if(this.valueArray[index] === undefined) {
+      return undefined;
     }
 
-    return this.valueArray[this.generateNumberFromString(key)];
+    const value = this.valueArray[index].filter((entry) => {
+      if(!entry) {
+        return false;
+      }
+      return entry[0] === key;
+    });
+
+    if(value.length === 0) {
+      return undefined;
+    }
+
+    return value[0][1];
   }
 
-  generateNumberFromString(str) {
+  getIndex(str) {
     let index = 0;
 
     for(let i = 0; i < str.length; i++) {
-      index += str[i].charCodeAt();
+      index += (str[i].charCodeAt() - 65) % 701 ;
     }
 
     return index;
   }
 }
+
+const ht = new HashTable();
+
+ht.add('m', 3);
+ht.add('a', 'hello there!');
+ht.remove('m');
+console.log(ht.get('hello'));
+console.log(ht.valueArray);
